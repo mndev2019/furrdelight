738f54a9ca9ce51840f693706e345c6e5cbd041d
mndev2019 <muskanvision2019@gmail.com>
2025-06-06 18:39:23 +0530

    c

--- src/Api/Baseurl.js
+++ src/Api/Baseurl.js
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 // export const baseUrl = "https://rinsezone.com:8953/"
-export const baseUrl = "https://10.5.49.29:8953/"
-=======
+// export const baseUrl = "https://10.5.49.29:8953/"
+
 // export const baseUrl = "http://213.210.36.202:8953/"
 export const baseUrl = "http://10.5.49.29:8953/"
->>>>>>> edae27ef3024923a9c877c51d1ad44d3fb03b087
+
